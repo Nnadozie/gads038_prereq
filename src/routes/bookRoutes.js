dff57478e75d162496db3a03150c3fb99e411f2d
Nnadozie Okeke <n.e.okeke@outlook.com>
2020-08-07 17:06:32 +0100

    done with sql

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -5,63 +5,42 @@ const debug = require('debug')('app:bookRoutes');
 const bookRouter = express.Router();
 
 function router(nav) {
-  const books = [
-    {
-      title: 'life of someone',
-      genre: 'History',
-      author: 'Mark Twain',
-      read: true,
-    },
-    {
-      title: 'life of someone',
-      genre: 'History',
-      author: 'Mark Twain',
-      read: true,
-    },
-    {
-      title: 'life of someone',
-      genre: 'History',
-      author: 'Mark Twain',
-      read: true,
-    },
-    {
-      title: 'life of someone',
-      genre: 'History',
-      author: 'Mark Twain',
-      read: true,
-    },
-    {
-      title: 'life of someone',
-      genre: 'History',
-      author: 'Mark Twain',
-      read: true,
-    },
-  ];
   bookRouter.route('/').get((req, res) => {
-    const request = new sql.Request();
+    (async function query() {
+      try {
+        const request = new sql.Request();
 
-    request
-      .query('select * from books')
-      .then((result) => {
-        debug(result);
+        const result = await request.query('select * from books');
         res.render('bookListView', {
           nav,
           title: 'Books',
           books: result.recordset,
         });
-      })
-      .catch((reas) => {
-        debug(reas);
-      });
+      } catch (error) {
+        debug(error);
+      }
+    })();
   });
 
   bookRouter.route('/:id').get((req, res) => {
-    const { id } = req.params;
-    res.render('bookView', {
-      nav,
-      title: 'Books',
-      book: books[id],
-    });
+    (async function query() {
+      try {
+        const { id } = req.params;
+        const request = new sql.Request();
+
+        const result = await request
+          .input('id', sql.Int, id)
+          .query('select * from books where id = @id');
+        debug(result);
+        res.render('bookView', {
+          nav,
+          title: 'Books',
+          book: result.recordset[0],
+        });
+      } catch (error) {
+        debug(error);
+      }
+    })();
   });
 
   return bookRouter;
