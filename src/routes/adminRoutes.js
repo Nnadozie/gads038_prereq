7693d1b0422b0b5e40dfebb423a49f3ae0b9533a
Nnadozie Okeke <n.e.okeke@outlook.com>
2020-08-07 17:30:04 +0100

    mongo insert

---
+++ src/routes/adminRoutes.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const { MongoClient } = require('mongodb');
+const debug = require('debug')('app:adminRoutes');
+
+const adminRouter = express.Router();
+
+const books = [
+  {
+    title: 'life of someone',
+    genre: 'History',
+    author: 'Mark Twain',
+    read: true,
+  },
+  {
+    title: 'life of someone',
+    genre: 'History',
+    author: 'Mark Twain',
+    read: true,
+  },
+  {
+    title: 'life of someone',
+    genre: 'History',
+    author: 'Mark Twain',
+    read: true,
+  },
+  {
+    title: 'life of someone',
+    genre: 'History',
+    author: 'Mark Twain',
+    read: true,
+  },
+  {
+    title: 'life of someone',
+    genre: 'History',
+    author: 'Mark Twain',
+    read: true,
+  },
+];
+
+module.exports = function router(nav) {
+  adminRouter.route('/').get((req, res) => {
+    const url = 'mongodb://localhost:27017';
+    const dbName = 'prereqDb';
+
+    (async function mongo() {
+      let client;
+      try {
+        client = MongoClient.connect(url);
+        debug('connected correctly to server');
+        const db = (await client).db(dbName);
+        const response = await db.collection('books').insertMany(books);
+        res.json(response);
+      } catch (error) {
+        debug(error);
+      }
+      (await client).close();
+    })();
+  });
+
+  return adminRouter;
+};
